17eec462c344cc55d7b2cf57a9cabf58b52f620f
bhuvireven007 <bhuvireven007@gmail.com>
2023-08-08 18:59:21 +0530

    changes made in component1

--- src/Components/component1.js
+++ src/Components/component1.js
@@ -5,19 +5,22 @@ class component1 extends Component {
     this.state = {
       color: "Green",
       name: "RAM",
+      game: "COD",
     };
   }
   Change() {
     this.setState({
       color: "BLACk",
       name: "Manoj",
+      game: "PUBG",
     });
   }
   render() {
     return (
       <>
         <p>
-          Hello {this.state.name} is favourtie color {this.state.color}
+          Hello {this.state.name} is favourtie color {this.state.color} and my
+          favorite game is {this.state.game}
         </p>
         <button onClick={() => this.Change()}>cc</button>
         <br></br>
